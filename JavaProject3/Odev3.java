aef7ff405d4bbf7cf2da8ff0a76a0d3cc492ae32
Ahmet Kutlay <ahmetkutlayr@gmail.com>
2022-10-28 10:56:02 +0300

    Odevler Eklendi

---
+++ JavaProject3/Odev3.java
@@ -0,0 +1,36 @@
+package JavaProject3;
+import java.util.Scanner;
+
+public class Odev3 {
+    public static void main(String[] args) {
+
+        int first , second ,third;
+        double alan,u;
+
+    Scanner kenar = new Scanner(System.in);
+    
+    System.out.println("İlk Kenar Uzunluğunu Giriniz");
+        first=kenar.nextInt();
+        
+    System.out.println("İkinci Kenar Uzunluğunu Giriniz");
+        second=kenar.nextInt();
+    
+   System.out.println("Üçüncü Kenar Uzunluğunu Giriniz");
+        third=kenar.nextInt();
+        
+            u=(first+second+third)/2;
+
+            alan=Math.sqrt(u*(u-first)*(u-second)*(u-third));
+
+
+    System.out.print("Üçgenin Alanı:"+alan);
+
+    
+   
+
+        
+    }
+    
+
+
+}
